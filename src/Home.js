7d5c963ea064b1908d58ce587bd4bbe1395fd94d
ihsanarifr <ihsan.android@gmail.com>
2022-02-21 06:39:40 +0700

    event click

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,21 @@
-import React from 'react';
-
 const Home = () => {
-  return ( 
+  const handleClick = () => {
+    console.log("hello, santekno");
+  }
+  const handleClickAgain = (name) => {
+    console.log("hello, " + name);
+  }
+  const handleClickAgainElement = (name, e) => {
+    console.log("hello, " + name, e);
+  }
+  return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Test</button>
+      <button onClick={() => handleClickAgain("ihsan")}>Click Me Again</button>
+      <button onClick={(e) => handleClickAgainElement("ihsan", e)}>Click Me Element</button>
     </div>
-   );
+  );
 }
- 
+
 export default Home;
