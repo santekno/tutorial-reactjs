9e7becaac41d61b2275822093bda6cebf2136a1b
ihsanarifr <ihsan.android@gmail.com>
2022-02-20 16:32:59 +0700

    styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 const Navbar = () => {
-  return ( 
+  return (
     <div className="navbar">
       <h1>Santekno Blog</h1>
       <div className="links">
         <a href="/">Beranda</a>
-        <a href="/create">Tambah Blog</a>
+        <a href="/create" style={{
+          color: "white",
+          backgroundColor: "#f1356d",
+          borderRadius: "8px",
+        }}>Tambah Blog</a>
       </div>
     </div>
-   );
+  );
 }
- 
+
 export default Navbar;
