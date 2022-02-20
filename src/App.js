7420dc3caceb8d9710d8ca58178372919b59dbd4
ihsanarifr <ihsan.android@gmail.com>
2022-02-20 15:47:18 +0700

    adding const value into tag

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.css';
 
 
 function App() {
+  const title = 'Selamat Datang di Santekno Blog';
+  const likes = 50;
+  const link = "https://google.com/";
+
   return (
     <div className="App">
       <div className='content'>
-        <h1>Santekno App Components</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
