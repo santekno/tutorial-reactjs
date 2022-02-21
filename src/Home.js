4d785ce38f3220deb7680dee29c6249ea7fee46a
ihsanarifr <ihsan.android@gmail.com>
2022-02-21 07:02:05 +0700

    implement useState

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 
 const Home = () => {
+  // let name = 'santekno';
+  const [name, setName] = useState('santekno');
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    // name = 'ihsan';
+    // console.log(name);
+    setName('ihsan');
+    setAge(29);
+  }
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <p>{ name } berumur { age } tahun</p>
+      <button onClick={handleClick}>Tekan ini</button>
     </div>
    );
 }
