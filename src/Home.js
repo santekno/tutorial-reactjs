5789bf80e7927a8eb95d6d6f4a9891b688bceb0f
ihsanarifr <ihsan.android@gmail.com>
2022-03-19 17:56:16 +0700

    adding fetch server json

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,15 @@
 import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'Hello World', body: 'lorem ipsum amet ...', author: 'santekno', id: 1 },
-    { title: 'Welcome to Santekno Blog', body: 'lorem ipsum amet ...', author: 'ihsan', id: 2 },
-    { title: 'Belajar ReactJs dengan Mudah', body: 'lorem ipsum amet ...', author: 'rafi', id: 3 },
-    { title: 'ReactJs Mudah dipelajari', body: 'lorem ipsum amet ...', author: 'santekno', id: 4 },
-  ]);
-
-  const [name,setName] = useState("ines");
-
-  const handleDelete = (id) => {
-    const newBlog = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlog);
-  }
-
-  useEffect(() => {
-    console.log("use effect ran");
-    console.log(name)
-  },[name]);
+  const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs")
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'santekno')} title="Santekno Blogs" handleDelete={handleDelete} />
-      <br />
-      <button onClick={()=> setName('nur hendriani')}>Ubah Nama</button>
-      <p>{ name }</p>
+      { error && <div>{ error }</div>}
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
     </div>
   );
 }
